5d8b0c1b1d97afe36dc70b29a61e6ea0a88e9b63
Nikmorking <nikmor.2010.king@gmail.com>
2026-01-28 22:23:20 +0300

    Я тут пояснять не обязан

---
+++ build/docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"org.example.plugin","l":"ExampleCommand"},{"p":"org.example.plugin","l":"ExampleHood"},{"p":"org.example.plugin","l":"ExamplePlugin"}];updateSearchResults();
